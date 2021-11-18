e3d9781fe3bc68cd968fe8a539073dc8a10553a7
Remon Dollenkamp <remon2004@outlook.com>
2021-11-18 14:32:56 +0100

    hoi

--- bibliotheek/mail.php
+++ bibliotheek/mail.php
@@ -1,61 +1,123 @@
 <?php
-// download fpdf class (http://fpdf.org)
-require("fpdf.php");
+//call the FPDF library
+require('fpdf.php');
 
-// fpdf object
-$pdf = new FPDF();
-
-// generate a simple PDF (for more info, see http://fpdf.org/en/tutorial/)
+$pdf = new FPDF('P','mm','A4');
+//add new page
 $pdf->AddPage();
-$pdf->SetFont("Arial","B",14);
-$pdf->Cell(40,10, "this is a pdf example");
+//set font to arial, bold, 14pt
+$pdf->SetFont('Arial','B',14);
 
-// email stuff (change data below)
-$to = "t88577457@gmail.com"; 
-$from = "me@domain.com"; 
-$subject = "send email with pdf attachment"; 
-$message = "<p>Please see the attachment.</p>";
+//Cell(width , height , text , border , end line , [align] )
 
-// a random hash will be necessary to send mixed content
+$pdf->Cell(130 ,5,'GEMUL APPLIANCES.CO',0,0);
+$pdf->Cell(59 ,5,'FACTUUR',0,1);//end of line
+
+//set font to arial, regular, 12pt
+$pdf->SetFont('Arial','',12);
+
+$pdf->Cell(130 ,5,'[Street Address]',0,0);
+$pdf->Cell(59 ,5,'',0,1);//end of line
+
+$pdf->Cell(130 ,5,'[City, Country, ZIP]',0,0);
+$pdf->Cell(25 ,5,'Date',0,0);
+$pdf->Cell(34 ,5,'[dd/mm/yyyy]',0,1);//end of line
+
+$pdf->Cell(130 ,5,'Phone [+12345678]',0,0);
+$pdf->Cell(25 ,5,'Invoice #',0,0);
+$pdf->Cell(34 ,5,'[1234567]',0,1);//end of line
+
+$pdf->Cell(130 ,5,'Fax [+12345678]',0,0);
+$pdf->Cell(25 ,5,'Customer ID',0,0);
+$pdf->Cell(34 ,5,'[1234567]',0,1);//end of line
+
+//make a dummy empty cell as a vertical spacer
+$pdf->Cell(189 ,10,'',0,1);//end of line
+
+//billing address
+$pdf->Cell(100 ,5,'Aan:',0,1);//end of line
+
+//add dummy cell at beginning of each line for indentation
+$pdf->Cell(10 ,5,'',0,0);
+$pdf->Cell(90 ,5, '-' ,0,1);
+
+$pdf->Cell(10 ,5,'',0,0);
+$pdf->Cell(90 ,5,'[Company Name]',0,1);
+
+$pdf->Cell(10 ,5,'',0,0);
+$pdf->Cell(90 ,5,'[Address]',0,1);
+
+$pdf->Cell(10 ,5,'',0,0);
+$pdf->Cell(90 ,5,'[Phone]',0,1);
+
+//make a dummy empty cell as a vertical spacer
+$pdf->Cell(189 ,10,'',0,1);//end of line
+
+//invoice contents
+$pdf->SetFont('Arial','B',12);
+
+$pdf->Cell(130 ,5,'Beschrijving',1,0);
+$pdf->Cell(25 ,5,'Aantal',1,0);
+$pdf->Cell(34 ,5,'Prijs',1,1);//end of line
+
+$pdf->SetFont('Arial','',12);
+
+//Numbers are right-aligned so we give 'R' after new line parameter
+
+$pdf->Cell(130 ,5,'Bier',1,0);
+$pdf->Cell(25 ,5,'-',1,0);
+$pdf->Cell(34 ,5,'?',1,1,'R');//end of line
+
+$pdf->Cell(130 ,5,'',0,0);
+$pdf->Cell(25 ,5,'Subtotaal',0,0);
+$pdf->Cell(4 ,5,'$',1,0);
+$pdf->Cell(30 ,5,'?',1,1,'R');//end of line
+
+
+
+$pdf->Cell(125 ,5,'',0,0);
+$pdf->Cell(30 ,5,'Verzendkosten',0,0);
+$pdf->Cell(4 ,5,'$',1,0);
+$pdf->Cell(30 ,5,'?',1,1,'R');//end of line
+
+$pdf->Cell(130 ,5,'',0,0);
+$pdf->Cell(25 ,5,'Totaalprijs',0,0);
+$pdf->Cell(4 ,5,'$',1,0);
+$pdf->Cell(30 ,5,'?',1,1,'R');//end of line
+
+
+$to = "t88577457@gmail.com";
+$subject = "mail with attachment";
+
 $separator = md5(time());
 
-// carriage return type (we use a PHP end of line constant)
 $eol = PHP_EOL;
 
 // attachment name
-$filename = "example.pdf";
 
-// encode data (puts attachment in proper format)
 $pdfdoc = $pdf->Output("", "S");
 $attachment = chunk_split(base64_encode($pdfdoc));
 
+$BOUNDARY="anystring";
 
-// main header (multipart mandatory)
-$headers  = "From: ".$from.$eol;
-$headers .= "MIME-Version: 1.0".$eol; 
-$headers .= "Content-Type: multipart/mixed; boundary=\"".$separator."\"".$eol.$eol; 
-$headers .= "Content-Transfer-Encoding: 7bit".$eol;
-$headers .= "This is a MIME encoded message.".$eol.$eol;
+$headers =<<<END
+From: Your Name <abc@gmail.com>
+Content-Type: multipart/mixed; boundary=$BOUNDARY
+END;
 
-// message
-$headers .= "--".$separator.$eol;
-$headers .= "Content-Type: text/html; charset=\"iso-8859-1\"".$eol;
-$headers .= "Content-Transfer-Encoding: 8bit".$eol.$eol;
-$headers .= $message.$eol.$eol;
+$body =<<<END
+--$BOUNDARY
+Content-Type: text/plain
 
-// attachment
-$message = "--".$uid.$eol;
-$message .= "Content-Type: text/html; charset=ISO-8859-1".$eol;
-$message .= "Content-Transfer-Encoding: 8bit".$eol.$eol;
-$message .= $body.$eol;
-$message .= "--".$uid.$eol;
-$message .= "Content-Type: application/pdf; name=\"".$filename."\"".$eol;
-$message .= "Content-Transfer-Encoding: base64".$eol;
-$message .= "Content-Disposition: attachment; filename=\"".$filename."\"".$eol;
-$message .= $content.$eol;
-$message .= "--".$uid."--";
+See attached file!
 
-// send message
-mail($to, $subject, $message, $headers);
+--$BOUNDARY
+Content-Type: application/pdf
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename="factuur.pdf"
 
-?>
+$attachment.$eol.$eol
+--$BOUNDARY--
+END;
+
+mail( $to, $subject, $body, $headers );
