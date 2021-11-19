0f24d5ea190b8e675a435e4ae530ed9da748c943
Remon Dollenkamp <remon2004@outlook.com>
2021-11-19 10:33:57 +0100

    hoi

--- bestel.php
+++ bestel.php
@@ -27,20 +27,3 @@ if ( $insert ) {
 
 $mysqli->close();
  }
-
-
-
-if (isset($_POST['submit'])) {
-    $name = $_POST['naam'];
-    $mailFrom = $_POST['email'];
-    $aantal = $_POST['aantal'];
-
-    $mailTo = "t88577457@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have recieved an e-mail from ".$name.".\n\n";
-
-    mail($mailTo, $txt, $aantal. 'bier');
-    header("Location: bibliotheek/factuur.php");
-
-}
-?>
--- bibliotheek/factuur.php
+++ bibliotheek/factuur.php
@@ -15,25 +15,21 @@ $pdf->SetFont('Arial','B',14);
 
 //Cell(width , height , text , border , end line , [align] )
 
-$pdf->Cell(130 ,5,'GEMUL APPLIANCES.CO',0,0);
+$pdf->Cell(130 ,5,'BIERBROUWERIJ DE BOER',0,0);
 $pdf->Cell(59 ,5,'FACTUUR',0,1);//end of line
 
 //set font to arial, regular, 12pt
 $pdf->SetFont('Arial','',12);
 
-$pdf->Cell(130 ,5,'[Street Address]',0,0);
+$pdf->Cell(130 ,5,'t88577457@gmail.com',0,0);
 $pdf->Cell(59 ,5,'',0,1);//end of line
 
-$pdf->Cell(130 ,5,'[City, Country, ZIP]',0,0);
+
 $pdf->Cell(25 ,5,'Date',0,0);
-$pdf->Cell(34 ,5,'[dd/mm/yyyy]',0,1);//end of line
+$pdf->Cell(34 ,5,date("d-m-Y"),0,1);//end of line
 
-$pdf->Cell(130 ,5,'Phone [+12345678]',0,0);
-$pdf->Cell(25 ,5,'Invoice #',0,0);
-$pdf->Cell(34 ,5,'[1234567]',0,1);//end of line
 
-$pdf->Cell(130 ,5,'Fax [+12345678]',0,0);
-$pdf->Cell(25 ,5,'Customer ID',0,0);
+$pdf->Cell(25 ,5,'Factuur #',0,0);
 $pdf->Cell(34 ,5,'[1234567]',0,1);//end of line
 
 //make a dummy empty cell as a vertical spacer
@@ -44,16 +40,16 @@ $pdf->Cell(100 ,5,'Aan:',0,1);//end of line
 
 //add dummy cell at beginning of each line for indentation
 $pdf->Cell(10 ,5,'',0,0);
-$pdf->Cell(90 ,5, '-' ,0,1);
+$pdf->Cell(90 ,5, $_POST['naam'] ,0,1);
 
 $pdf->Cell(10 ,5,'',0,0);
-$pdf->Cell(90 ,5,'[Company Name]',0,1);
+$pdf->Cell(90 ,5,$_POST['adres'],0,1);
 
 $pdf->Cell(10 ,5,'',0,0);
-$pdf->Cell(90 ,5,'[Address]',0,1);
+$pdf->Cell(90 ,5,$_POST['postcode'],0,1);
 
 $pdf->Cell(10 ,5,'',0,0);
-$pdf->Cell(90 ,5,'[Phone]',0,1);
+$pdf->Cell(90 ,5,$_POST['email'],0,1);
 
 //make a dummy empty cell as a vertical spacer
 $pdf->Cell(189 ,10,'',0,1);//end of line
@@ -61,7 +57,7 @@ $pdf->Cell(189 ,10,'',0,1);//end of line
 //invoice contents
 $pdf->SetFont('Arial','B',12);
 
-$pdf->Cell(130 ,5,'Beschrijving',1,0);
+$pdf->Cell(130 ,5,'Omschrijving',1,0);
 $pdf->Cell(25 ,5,'Aantal',1,0);
 $pdf->Cell(34 ,5,'Prijs',1,1);//end of line
 
@@ -73,7 +69,6 @@ $pdf->Cell(130 ,5,'Bier',1,0);
 $pdf->Cell(25 ,5,'-',1,0);
 $pdf->Cell(34 ,5,'?',1,1,'R');//end of line
 
-//summary
 $pdf->Cell(130 ,5,'',0,0);
 $pdf->Cell(25 ,5,'Subtotaal',0,0);
 $pdf->Cell(4 ,5,'$',1,0);
